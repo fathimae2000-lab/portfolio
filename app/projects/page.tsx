15c2c8cba67b4d3bbd0ea2192616202ee837d366
Fathima <fathimae2000@gmail.com>
2026-07-11 23:13:17 +0530

    added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -32,7 +32,7 @@ const projects = [
       { name: "MongoDB" },
     ],
     image: "/crm.png",
-    live: "https://crm-software-frontend-8x45jvgvm-fathimae2000-labs-projects.vercel.app",
+    live: "https://crm-software-frontend-git-master-fathimae2000-labs-projects.vercel.app/",
     github: "https://github.com/fathimae2000-lab/crm_software_frontend",
   },
    {
